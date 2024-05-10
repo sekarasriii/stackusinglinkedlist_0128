0e34ccca67ce871fab1a561c3083f7cf630c786f
sekarasriii <sekareeesh@gmail.com>
2024-05-10 10:07:49 +0700

    membuat prosedur peek/top

--- Act7_StackC++/Act7_StackC++.cpp
+++ Act7_StackC++/Act7_StackC++.cpp
@@ -31,6 +31,14 @@ public:
 	}
 
 	void pop() {
+		if (isEmpty()) {
+			cout << "Stack is empty." << endl;
+		}
+		cout << "Popped value : "<< top->data << endl;
+		top = top->next;
+	}
+
+	void peek() {
 
 	}
 };
