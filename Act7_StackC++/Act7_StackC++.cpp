99527487a05e6d2e49765a9a1cc31586c8e577ab
sekarasriii <sekareeesh@gmail.com>
2024-05-10 10:11:05 +0700

    membuat fungsi isEmpty dengan tipe data boolean

--- Act7_StackC++/Act7_StackC++.cpp
+++ Act7_StackC++/Act7_StackC++.cpp
@@ -39,6 +39,20 @@ public:
 	}
 
 	void peek() {
+		if (top == NULL) {
+			cout << "List is empty." << endl;
+		}
+		else {
+			Node* current = top;
+			while (current != NULL) {
+				cout << current->data << " " << endl;
+				current = current->next;
+			}
+			cout << endl;
+		}
+	}
 
+	bool isEmpty() {
+		return top == NULL;
 	}
 };
